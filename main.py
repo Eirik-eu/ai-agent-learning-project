27ab01728a8856fabc2e2ab66f985591591ea1fe
Eirik <github.com.earthworm256@passmail.com>
2025-08-19 21:15:34 +0200

    Added feedback loop to the agent

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ def main():
     if len(sys.argv) < 2:
         print("Need to input a prompt.")
         exit(1)
-
     
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
@@ -32,7 +31,11 @@ def main():
     - Execute Python files with optional arguments
     - Write or overwrite files
 
+    If the user doesn't specify an exact filepath you should start by looking through the available files to understand which one it they are referring to. 
+    If you don't find the file, you should call get_files_info on discovered directories to see what's inside and dig deeper.
+    Don't assume that a file with a different name is the correct one until after you've looked through all subdirectories.
     All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    When you are finished you should no longer need to run any more functions.
     """
 
     available_functions = types.Tool(
@@ -47,34 +50,48 @@ def main():
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)])
     ]
-    
-    response = client.models.generate_content(
-        model="gemini-2.0-flash-001",
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions],
-            system_instruction=system_prompt
-        ),
-    )
 
     verbose = False
     if len(sys.argv) == 3:
             if sys.argv[2] == "--verbose":
-                 verbose = True
+                verbose = True
 
-    prompt_tokens = response.usage_metadata.prompt_token_count
-    response_tokens = response.usage_metadata.candidates_token_count
+    try:
+        for i in range(20):
+            response = client.models.generate_content(
+                model="gemini-2.0-flash-001",
+                contents=messages,
+                config=types.GenerateContentConfig(
+                    tools=[available_functions],
+                    system_instruction=system_prompt
+                ),
+            )
+            
+            prompt_tokens = response.usage_metadata.prompt_token_count
+            response_tokens = response.usage_metadata.candidates_token_count
 
-    if verbose:
-        print("Prompt tokens:", prompt_tokens)
-        print("Response tokens:", response_tokens)
+            if verbose:
+                print("\nPrompt tokens:", prompt_tokens, "\n")
+                print("Response tokens:", response_tokens, "\n")
 
-    if response.function_calls:
-        function_call_result = call_function(response.function_calls[0], verbose)
-        print(f"-> {function_call_result.parts[0].function_response.response}")
-    else:
-        print(response.text)
-    
+            if response.function_calls:
+                print(f"\nLoop number {i+1}\nCurrent thoughts:\n{response.text}\n")
+                function_call_result = call_function(response.function_calls[0], verbose)
+                print(f"-> {function_call_result.parts[0].function_response.response}")
+                messages.append(response.candidates[0].content) 
+                messages.append(types.Content(
+                    role="user", 
+                    parts=[types.Part(text=f"Here is the result of the previous function: {str(function_call_result.parts[0].function_response.response)}")]
+                ))
+            else:
+                print(f"\nLoop number {i+1}\nFinal answer:\n{response.text}\n")
+                return
+            
+            #print(f"\nTesting Messages output: {messages}")
+            #print(f"\nCandidate text: {response.candidates[0].content.parts[0]}\n")
+            #print(f"Response: {response.candidates[0].finish_message}")
+    except Exception as e:
+        print(f"Error {e}")
 
 if __name__ == "__main__":
     main()
